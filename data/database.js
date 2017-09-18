6e9a2c1ae4907e0073e020d030f34ed297e3f73c
snakeful <fulblind@gmail.com>
2017-09-18 06:32:06 -0600

    Change connection data to database.

--- data/database.js
+++ data/database.js
@@ -1,3 +1,30 @@
-module.exports = function (config) {
-  return require('knex')(config);
+module.exports = function () {
+  const pgError = require('pg-error');
+  return require('knex')({
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '50l1d5nAk3',
+      database : 'fulscrum'
+    },
+    pool: {
+      min: 1,
+      max: 50,
+      afterCreate: function(conn, done) {
+        conn.connection.parseE = pgError.parse
+        conn.connection.parseN = pgError.parse
+        conn.connection.on('PgError', function (err) {
+          switch (err.severity) {
+            case 'ERROR':
+            case 'FATAL':
+            case 'PANIC': return this.emit('error', err);
+            default: return this.emit('notice', err);
+          }
+        });
+        done();
+      }
+    }
+  });
 };
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { express, server } = require('ful-ms-svr');
-const pgError = require('pg-error');
 let port = 3000;
 console.log(`Ejecutando servidor SCRUM. ${process.pid}`);
 server.port = port;
@@ -8,33 +7,7 @@ server.run(port);
 let router = express.Router();
 server.use(express.static('public'));
 server.use('/api/scrum', router);
-let db = require('./data/database')({
-  client: 'pg',
-  version: '9.6',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '50l1d5nAk3',
-    database : 'fulscrum'
-  },
-  pool: {
-    min: 1,
-    max: 50,
-    afterCreate: function(conn, done) {
-      conn.connection.parseE = pgError.parse
-      conn.connection.parseN = pgError.parse
-      conn.connection.on('PgError', function (err) {
-        switch (err.severity) {
-          case 'ERROR':
-          case 'FATAL':
-          case 'PANIC': return this.emit('error', err);
-          default: return this.emit('notice', err);
-        }
-      });
-      done();
-    }
-  }
-});
+let db = require('./data/database')();
 require('walk-sync')('./logic/project').filter(function(file) {
   return (/\.js$/i).test(file);
 }).map(function(file) {
