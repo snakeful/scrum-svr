bbd7c33dc9c251e16b7efcd2b4e31f660c4daaba
snakeful <fulblind@gmail.com>
2017-09-27 00:43:51 -0600

    Add 404 with json error.

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ server.run(port);
 let router = express.Router();
 server.use(express.static('public'));
 require('./logic/project/resource')(server);
+server.get('*', (req, res, next) => {
+  res.status(404).json({
+    err: 'Url not found on FUL-SCRUM server'
+  });
+})
