fec91d3e01024f58403fd415015c29d433a016ee
Carlos Ful <cful@tc.tecun.net.gt>
2017-10-04 08:15:30 -0600

    Add sprint controlelr to resource.

--- logic/project/resource.js
+++ logic/project/resource.js
@@ -3,4 +3,5 @@ module.exports = function (server) {
   server.use('/api', require('./users'));
   server.use('/api', require('./projects'));
   server.use('/api', require('./user-stories'));
+  server.use('/api', require('./sprints'));
 }
