e6dab05f22836ec98f35c9622b887fb237bf5cfc
Carlos Ful <cful@tc.tecun.net.gt>
2017-10-16 12:43:27 -0600

    :mute: Remove log of $entity in burndown-chart.js.

--- logic/project/burndown-chart.js
+++ logic/project/burndown-chart.js
@@ -1,6 +1,5 @@
 const $router = require('express').Router();
 const $entity = require('../../data/entity/burndown-chart');
-console.log($entity);
 $router.get(`/${$entity.resource}`, (req, res) => {
   $entity.getBurndownChart(req, res).then(data => {
     res.json(data.rows);
