a3248ebfa80c4174f0720b42e0d75f64ff9ea4d2
snakeful <fulblind@gmail.com>
2017-10-17 20:11:26 -0600

    :art: Add param to burndown chart call.

--- data/entity/burndown-chart.js
+++ data/entity/burndown-chart.js
@@ -4,7 +4,7 @@ const entity = {
   resource: 'burndown-charts'
 };
 entity.getBurndownChart = function (req, res) {
-  const sprintId = 1;//parseInt(req.query.sprintId, 0);
+  const sprintId = req.params.id;
   const date = `'${req.query.date || new Date().toISOString().substring(1, 10)}'`;
   const sql = fs.readFileSync('./data/entity/query/burndown-chart.sql').toString();
   return $db.raw(sql, [sprintId, sprintId, sprintId, date]);
--- logic/project/burndown-chart.js
+++ logic/project/burndown-chart.js
@@ -1,6 +1,6 @@
 const $router = require('express').Router();
 const $entity = require('../../data/entity/burndown-chart');
-$router.get(`/${$entity.resource}`, (req, res) => {
+$router.get(`/${$entity.resource}/:id`, (req, res) => {
   $entity.getBurndownChart(req, res).then(data => {
     res.json(data.rows);
   }).catch(err => {
