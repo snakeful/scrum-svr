b028ef0e2e9a5ae57135d034fec025a0c3afe179
snakeful <fulblind@gmail.com>
2017-10-22 05:58:48 -0600

    :art: Update sprint user stoires to use database transactions.

--- logic/project/sprint-user-stories.js
+++ logic/project/sprint-user-stories.js
@@ -1,22 +1,22 @@
 const $router = require('express').Router();
 const $entity = require('../../data/entity/sprint-user-stories');
 const $entityUserStory = require('../../data/entity/user-stories');
-function updateUserStory (userStoryId, statusId) {
+function updateUserStory (trx, userStoryId, statusId) {
   userStoryId = parseInt(userStoryId, 0);
   return new Promise((resolve, reject) => {
     $entityUserStory.getById({}, userStoryId).then(userStory => {
       userStory[0].statusId = statusId;
-      $entityUserStory.update(userStory[0]).then(() => {
+      $entityUserStory.update(trx, userStory[0]).then(() => {
         resolve();
       }, reject);
     }, reject);
   });
 };
-$entity.onInsert = (obj, data) => {
-  return updateUserStory(obj.userStoryId, 1);
+$entity.afterInsert = (trx, obj, data) => {
+  return updateUserStory(trx, obj.userStoryId, 1);
 };
 $entity.onDelete = query => {
-  return updateUserStory(query.where.userStoryId, 0);
+  return updateUserStory(trx, query.where.userStoryId, 0);
 };
 require('../resource')($router, $entity);
 module.exports = $router;
